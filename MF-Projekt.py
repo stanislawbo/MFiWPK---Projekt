a6f33a25068fe0233cb27602e7bd18a6517c5398
Stanislaw <stanislawboczarski@gmail.com>
2026-05-18 14:01:06 +0200

    operatory

--- MF-Projekt.py
+++ MF-Projekt.py
@@ -1,2 +1,11 @@
-import math
-print(math.sqrt(6561))
+def K(a, b): return a and b
+def A(a, b): return a or b
+def I(a, b): return (not a) or b
+def R(a, b): return a == b
+
+OPS = {
+    "k": (2, K),
+    "a": (1, A),
+    "i": (0, I),
+    "r": (0, R),
+}
