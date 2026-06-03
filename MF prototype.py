26f5910d9c67da1db5980776c78295b56bef6293
dygasd <ddygas03@gmail.com>
2026-06-03 14:47:59 +0200

    Komentarze + spójność języka
    Dodano komentarze do kodu i funkcji oraz zmieniono ich język na polski

--- MF prototype.py
+++ MF prototype.py
@@ -1,11 +1,9 @@
-import random
-
 counter = 1
 def declare_variable(variable_name):
     """
-    Creates a new variable to use in logical formulas
-    :param variable_name: Variable name
-    :return: Null
+    Deklaracja zmiennej, której można później używać w formułach logicznych
+    :param variable_name: Nazwa zmiennej
+    :return:
     """
     global counter
     Logical.variables[variable_name] = counter
@@ -14,7 +12,19 @@ def declare_variable(variable_name):
 class Logical:
     variables = {}
     def __init__(self, left = None, op = None, right = None, name = None, clauses = []):
-        if op is None:
+        """
+        Definicja wyrażenia logicznego
+        :param left: Lewa strona wyrażenia (pusta w przypadku negacji i wyrażeń będących jedną zmienną)
+        :param op: Operator, jeden z &, |, =>, ~ (puste w przypadku wyrażeń będących jedną zmienną)
+        :param right: Prawa strona wyrażenia (negowana formuła w przypadku negacji i zmienna dla wyrażeń będących jedną zmienną)
+        :param name: Nazwa formuły, potrzebna tylko dla pojedynczych zmiennych, do ich wypisywania
+        :param clauses: Klauzule, które wchodzą w skład formuły
+        """
+
+        if op not in ('&', '|', '=>', '~', None):
+            print('System: Nieznany operator')
+
+        elif op is None:  # Wyrażenia będące pojedynczą zmienną
             declare_variable(self)
             self.left = None
             self.op = None
@@ -22,17 +32,14 @@ class Logical:
             self.name = name
             self.clauses = [self]
 
-        elif op not in ('&', '|', '=>', '~'):
-            print('Can\'t create formulas without operators / unknown operator')
-
-        elif op == '~':
+        elif op == '~':  # Negacje wyrażeń
             self.left = None
             self.op = op
             self.right = right
             self.name = name
             self.clauses = clauses
 
-        elif op in ('|', '&', '=>'):
+        elif op in ('|', '&', '=>'):  # Koniunkcje, alternatywy i implikacje
             self.left = left
             self.op = op
             self.right = right
@@ -43,6 +50,11 @@ class Logical:
             print('Error: Something went wrong. Formula not created')
 
     def __str__(self, cnf = False):
+        """
+        Definicja sposobu wypisywania formuł
+        :param cnf: Czy formuła jest w postaci CNF; domyślnie False
+        :return: Formuła w postaci napisu
+        """
         # Need to fix extra parentheses in multi-level formulas
         if cnf:
             cnf_form = self.clauses[0]
@@ -50,162 +62,182 @@ class Logical:
                 cnf_form = cnf_form | clause
             return cnf_form
 
-        if self.op is None:
+        if self.op is None:  # Pojedyncze zmienne wypisujemy jako ich atrybuty "name"
             return self.name
-        if self.op == '~':
-            if self.right.op == '~':
+        if self.op == '~':  # W przypadku negacji...
+            if self.right.op == '~':  # jeżeli negowane wyrażenie samo w sobie jest negacją, wyrzucamy obie te negacje...
                 return str(self.right.right)
-            return f'~{self.right}'
-            return f'~{self.right}' if self.right.right == self.right else f'~({self.right})'
-        return f'({self.left} {self.op} {self.right})'
+            return f'~{self.right}'  # a jeżeli nie, to przed napisem formuły doklejamy znak ~
 
-    def __repr__(self):
+        return f'({self.left} {self.op} {self.right})'  # W pozostałych przypadkach rekurencyjnie wypisujemy lewą i prawą
+                                                        # stronę formuły z operatorem pomiędzy nimi
+
+    def __repr__(self):  # Wypisywanie zmiennych w tablicach, np. w atrybucie clauses
         return str(self)
 
     def __and__(self, other):
-        if self == T:
+        """
+        Nadpisanie operatora "&" jako spójnika logicznego "and"
+        :param other: Druga formuła, która ma zostać połączona z pierwszą za pomocą spójnika "&"
+        :return: Koniunkcja obu formuł
+        """
+        if self == T:  # Koniunkcja z prawdą nie zmienia wartości formuły
             return other
-        if len(self.clauses) > 0 and len(other.clauses) > 0:
-            return Logical(self, '&', other, clauses = (self.clauses + other.clauses))
-        return Logical(self, '&', other)
+
+        if len(self.clauses) > 0 and len(other.clauses) > 0:  # Jeżeli obie formuły są w postaci CNF, to ich koniunkcja również
+            return Logical(self, '&', other, clauses = (self.clauses + other.clauses))  # a klauzule koniunkcji to klauzule
+                                                                                            # z obu formuł
+        return Logical(self, '&', other)  # W pozostałych przypadkach zwraca koniunkcję bez dodatków
 
     def __or__(self, other):
-        if self == F:
+        """
+        Nadpisanie operatora "|" jako spójnika logicznego "or"
+        :param other: Druga formuła, która ma zostać połączona z pierwszą za pomocą spójnika "|"
+        :return: Alternatywa obu formuł
+        """
+        if self == F:  # Alternatywa z fałszem nie zmienia wartości formuły
             return other
-        if len(self.clauses) == len(other.clauses) == 1:
+
+        if len(self.clauses) == len(other.clauses) == 1:  # Alternatywa klauzul jest jedną klauzulą
             return Logical(self, '|', other, clauses = [Logical(self.clauses[0], '|', other.clauses[0])])
-        return Logical(self, '|', other)
+
+        return Logical(self, '|', other)  # W pozostałych przypadkach zwraca alternatywę bez dodatków
 
     def __rshift__(self, other):
+        """
+        Nadpisanie operatora ">>" jako spójnika logicznego modelującego implikację
+        :param other: Następnik implikacji
+        :return: Implikacja obu formuł
+        """
         return Logical(self, '=>', other)
 
     def __invert__(self):
-        if self in Logical.variables:
+        """
+        Nadpisanie operatora "~" jako spójnika logicznego modelującego implikację
+        :return: Negacja formuły
+        """
+        if self in Logical.variables:  # Jeżeli negujemy zmienną, to jej negacja jest również klauzulą
             return Logical(None, '~', self, clauses = [Logical(None, '~', self.clauses[0])])
-        elif self.op == '~' and self.right in Logical.variables:
-            return Logical(None, None, self.right, name = self.right.right.name, clauses = [Logical(None, None, None, self.right.clauses[0])])
-        return self.right if self.op == '~' else Logical(None, '~', self)
+        
+        elif self.op == '~' and self.right in Logical.variables:  # Jeżeli negujemy negację zmiennej, zwraca tę zmienną
+            return self.right
+        
+        return self.right if self.op == '~' else Logical(None, '~', self)  # W pozostałych przypadkach zwracamy negację
+                                                                                    # z uwzględnieniem przypadku podwójnej negacji
 
     def eliminate_implication(self):
-        if self.op is None:
+        """
+        Funkcja zamieniająca w formule wszystkie implikacje za pomocą alternatyw
+        :return: Równoważna postać formuły bez użycia operatora implikacji
+        """
+        if self.op is None:  # Zwracamy zmienne
             return self
-        elif self.op == '&':
+        elif self.op == '&':  # Koniunkcje, alternatywy i negacje traktujemy rekurencyjnie
             return Logical.eliminate_implication(self.left) & Logical.eliminate_implication(self.right)
         elif self.op == '|':
             return Logical.eliminate_implication(self.left) | Logical.eliminate_implication(self.right)
         elif self.op == '~':
             return ~Logical.eliminate_implication(self.right)
+        
+        # W pozostałym przypadku (implikacja), zastępujemy formułę "X => Y" przez "~X | Y" i rekurencyjnie przechodzimy głebiej
         return ~Logical.eliminate_implication(self.left) | Logical.eliminate_implication(self.right)
 
     def distribute_or(self):
-        if len(self.clauses) == 1:
+        """
+        Funkcja korzystająca z praw rozdzielności dla koniunkcji formuł w postaci CNF
+        :return: Formuła wejściowa w równoważnej postaci CNF
+        """
+        if len(self.clauses) == 1:  # Dodane, żeby pozbyć się błędu, potencjalnie niepotrzebne w przyszłości
             return self
-        # Requires the formula to be in a form CNF | CNF
-        res = T
+        res = T  # Startujemy z pustej formuły...
         for cl in self.left.clauses:
             for cr in self.right.clauses:
-                new_clause = cl | cr
-                res &= new_clause
+                new_clause = cl | cr  # i dla każdej pary klauzul z lewej i prawej części dorzucamy
+                res &= new_clause     # ich alternatywę do wyniku
         return res
 
-    def distribute_orr(self):
-        # Requires the formula to be in a form CNF | CNF
-        # Błąd z podwajaniem jedynej pary klauzul, jeżelijest tylko jedna
-        print('Formula to distribute or:', self)
-        res = self.left.clauses[0] | self.right.clauses[0]
-        clauses = [self.left.clauses[0] | self.right.clauses[0]]
-        for i in range(len(self.left.clauses)):
-            for j in range(len(self.right.clauses)):
-                if (i == 0 and j == 0) or (i == len(self.left.clauses) - 1 and j == len(self.right.clauses) - 1):
-                    continue
-                res = res & (self.left.clauses[i] | self.right.clauses[j])
-                clauses.append(self.left.clauses[i] | self.right.clauses[j])
-        clauses.append(self.left.clauses[-1] | self.right.clauses[-1])
-        return Logical(res, '&', self.left.clauses[-1] | self.right.clauses[-1], clauses = clauses)
-
     def remove_negation(self):
-        # Requires the formula to be in a form ~(x | y) or ~(x & y)
-        if self.op != '~':
+        """
+        Funkcja przerzucająca w formule negacje na poziom pojedynczych zmiennych
+        :return: Równoważna postać formuły wejściowej, z negacjami tylko bezpośrednio przy zmiennych
+        """
+        if self.op != '~':  # Usuwamy podwójne negacje
             return self
-        elif self.right.op is None:
+        elif self.right.op is None:  # Jeżeli negacja zmiennej, zwracamy ją
             return self
-        elif self.right.op == '&':
+        elif self.right.op == '&':  # Dla koniunkcji i alternatywy stosujemy prawa de Morgana
             return ~self.right.left | ~self.right.right
         elif self.right.op == '|':
             return ~self.right.left & ~self.right.right
         else:
-            print(self)
-            raise Exception('Can\'t handle that formula')
+            raise Exception('Błąd: Niepoprawna formuła. Spróbuj najpierw usunąć implikacje')# Mak
 
     def to_cnf(self):
-        no_impl = Logical.eliminate_implication(self)
-        # Going from outer-most symbol, if it's a:
-        # > & - leave as it is, convert left and right to cnf
-        if no_impl.op == '&':
+        """
+        Funkcja przekształcająca formułę do postaci CNF
+        :return: Równoważna formuła w postaci CNF
+        """
+        no_impl = Logical.eliminate_implication(self)  # Usuwamy implikacje z formuły
+        # Przechodzi rekurencyjnie po formule
+        if no_impl.op == '&':  # Jeżeli jest ona koniunkcją, wystarczy obie strony sprowadzić do postaci CNF
             return Logical.to_cnf(no_impl.left) & Logical.to_cnf(no_impl.right)
-        elif no_impl.op == '|':
+        elif no_impl.op == '|':  # Jeżeli jest alternatywą, to obie strony sprowadza do postaci CNF i łączy prawem rozdzielności
             return Logical.distribute_or(Logical.to_cnf(no_impl.left) | Logical.to_cnf(no_impl.right))
-        elif no_impl.op == '~':
-            if no_impl.right.op is None:
+        elif no_impl.op == '~':  # Jeżeli jest negacją, to...
+            if no_impl.right.op is None:  # albo jest negacją zmiennej i w takim przypadku zwraca tę negację, ...
                 return no_impl
-            return Logical.to_cnf(Logical.remove_negation(no_impl))
+            return Logical.to_cnf(Logical.remove_negation(no_impl))  # albo sprowadza do CNF po przełożeniu negacji na niższy poziom
         else:
-            return no_impl
+            return no_impl  # Zwracamy pojedyncze zmienne
 
 T = Logical(name = 'True')
 F = Logical(name = 'False')
 x, y, z = Logical(name = 'x'), Logical(name = 'y'), Logical(name = 'z')
-# phi = Logical(A, '=>', K, name = 'phi')
-# print('phi = (x & y) => (z | ~x)')
-# phi = x | y >> (z & ~x)
-# print('phi =', phi)
-#
-print('Test 0:', Logical.to_cnf(x))
-print('Test 0.(9)8:', Logical.to_cnf((x | y) & z))
-print('Test 0.(9):', Logical.to_cnf((x & y) | z))
-print('Test 1:', Logical.to_cnf(~(x | (y & z))))
-print('Test 2:', Logical.to_cnf((x & y) | (~x & z)))
-print('Test 3:', Logical.to_cnf(x >> (y >> z)))
-print('Test 3.1:', Logical.to_cnf(x >> (~y >> z)))
-# print((~x).clauses, (~y | z).clauses)
+
+print(~(~y))
 
 while True:
-    cmd_full = input('')
-    cmd = cmd_full.split()
-    if cmd[0] == 'var':
-        if len(cmd) == 2:
+    cmd_full = input('')  # Wczytywanie inputu użytkownika z konsoli ...
+    cmd = cmd_full.split()  # i dzielenie na części rozdzielone spacjami
+    
+    if cmd[0] == 'var':  # Jeżeli pierwszą częścią jest słowo kluczowe "var", użytkownik chce zadeklarować zmienne
+        if len(cmd) == 2:  # Jeżeli użytkownik deklaruje tylko jedną zmienną, nadajemy jej nazwę taką, jak podał użytkownik
             exec(f'{cmd[1]} = Logical(name = \'{cmd[1]}\')')
-            print(f'System: Declared variable {cmd[1]}')
-        elif cmd[2] == 'for' and cmd[5] == 'to' and len(cmd) == 7:
-            range_l = int(cmd[4])
+            print(f'System: Zadeklarowano zmienną {cmd[1]}')
+            
+        elif cmd[2] == 'for' and cmd[5] == 'to' and len(cmd) == 7:  # Jeżeli użytkownik chce zadeklarować kilka zmiennych naraz ...
+            range_l = int(cmd[4])  # zczytujemy granice indeksownia
             range_h = int(cmd[6])
-            if range_h < range_l:
-                print('System: Incorrect range')
+            if range_h < range_l:  # Sprawdzenie poprawności zakresu
+                print('System: Niepoprawny zakres')
                 continue
-            idx = cmd[3]
+            idx = cmd[3]  # To słowo służy jako indeks i będzie nadpisane liczbami z podanego zakresu
             idx_in_var = cmd[1].find(idx)
-            if idx_in_var == -1:
+            if idx_in_var == -1:  # Podany indeks musi być obecny w nazwie zmiennej
                 print('System: Variable not indexed with given string')
                 continue
-            vars = [cmd[1][:idx_in_var] + str(i) + cmd[1][idx_in_var + len(cmd[3]):] for i in
-                    range(range_l, range_h + 1)]
-            for var in vars:
+            # Jeżeli wszystko pasuje, tworzymy tablicę nazw zmiennych ...
+            vars = [cmd[1][:idx_in_var] + str(i) + cmd[1][idx_in_var + len(cmd[3]):] for i in range(range_l, range_h + 1)]
+            for var in vars:  # i deklarujemy każdą z osobna
                 exec(f'{var} = Logical(name = \'{var}\')')
-            print(f'System: Declared variables {vars[0]} to {vars[-1]}')
+            print(f'System: Zadeklarowano zmienne {vars[0]}, ..., {vars[-1]}')
+            
+        else:  # Niepoprawna składnia prowadzi do błędu
+            print('System: Niepoprawna składnia')
 
-    elif cmd[0] == 'print':
-        if len(cmd) == 3 and cmd[2] == 'cnf':
+    elif cmd[0] == 'print':  # Jeżeli polecenie zaczyna się od słowa kluczowego "print", użytkownik chce wypisać formułę
+        if len(cmd) == 3 and cmd[2] == 'cnf':  # Jeżeli chce, wypisuje ją w postaci CNF
             exec(f'print({cmd[1]}, True)')
-        elif len(cmd) == 2:
+        elif len(cmd) == 2:  # W przeciwnym przypadku wypisuje ją normalnie
             exec(f'print({cmd[1]})')
-        else:
-            print('Error')
+        else:  # Niepoprawna składnia prowadzi do błędu
+            print('System: Błąd składni')
 
-    elif cmd[0] == 'end':
+    elif cmd[0] == 'end':  # Jeżeli polecenie to "end", użytkownik chce zakończyć pracę z programem
         print('System: Ending current run')
         break
 
-    elif len(cmd) > 2 and cmd[1] == '=':
+    elif len(cmd) > 2 and cmd[1] == '=':  #  tym przypadku, użytkownik chce utorzyć formułę logiczną
         ''' ----------========== To prawie na pewno da się lepiej ==========---------- '''
         formula = ''.join(cmd[2:]).replace('=>', '>>')
         print(formula)
@@ -240,13 +272,21 @@ while True:
             missing = str(e).split("'", 2)[1]
             print(f'Error: Variable {missing} not declared')
 
-    elif cmd[0] in ['help', '?'] and len(cmd) == 1:
-        print('Use \'var <variable_name>\' to declare variables')
-        print('Use \'var <indexed_variable_name> for <idx> n to m\' to declare many variables at once\n'
-              '    Example: var x_i for i 1 to 5')
-        print('Use \'print <variable / formula name>\' to print it')
-        print('Use \'end\' to terminate the program')
+    elif cmd[0] in ['help', '?'] and len(cmd) == 1:  # Jeżeli polecenie to "help" lub "?", wyświetla pomoc dotyczącą możliwych poleceń
+        print('Użyj \'var <nazwa zmiennej>\', żeby zadeklarować zmienną')
+        print('Użyj \'var <nazwa zmiennej z indeksem> for <indeks> n to m\', żeby zadeklarować kilka zmiennych naraz\n'
+              '    Przykład: var x_i for i 1 to 5')
+        print('Użyj \'print <nazwa zmiennej / formuły>\', żeby ją wypisać')
+        print('Użyj \'end\', żeby zamknąć program')
 
     else:
-        #exec(cmd_full)
-        print('System: Unknown command')
+        print('System: Nieznane polecenie')
+        
+"""
+Do zrobienia
+ > rekurencyjna wersja remove_negation
+ > usunięcie powielonych nawiasów przy wypisywaniu formuł
+ > poprawne wypisywanie formuł w postaci CNF
+ > dodać implikacje do remove_negation
+ > zoptymalizować tworzenie formuł po inpucie użytkownika
+"""
